21b6e3a5f5376ae3af413b89f02882945d5be350
ranitchakraborty1 <162864925+ranitchakraborty1@users.noreply.github.com>
2024-08-09 11:20:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
 function volume_sphere() {
     //Write your code here
-  
+
+	  const radius = document.getElementById('radius').value;
+	
+
+	   if (radius === '' || isNaN(radius) || radius <= 0) {
+        alert('Please enter a valid, non-negative radius');
+        return;
+    }
+
+   const volume = (4 / 3) * Math.PI * Math.pow(radius, 3);
+	const roundedVolume = volume.toFixed(4);
 } 
 
 window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
